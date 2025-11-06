799eccaed2216676816c583df6b284d6f2fdc211
Arindam Mitra <arindammitra06@users.noreply.github.com>
2025-11-06 21:43:40 +0530

    Ignore warnings

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
+  eslint: {
+    ignoreDuringBuilds: true, // ignores all ESLint warnings during build
+  },
 };
 
 export default nextConfig;
