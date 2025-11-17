de02374bdc3a8b86e8ddc1d078bf81d49861d364
Arindam Mitra <arindammitra06@users.noreply.github.com>
2025-11-17 14:25:22 +0530

    NEXT_PUBLIC_ added

--- src/utils/imagekitClient.ts
+++ src/utils/imagekitClient.ts
@@ -2,9 +2,9 @@ import ImageKit from "imagekit";
 
 
 const imagekit = new ImageKit({
-  publicKey: process.env.IMAGEKIT_PUBLIC_KEY!,
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
-  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT!,
+  publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+  privateKey: process.env.NEXT_PUBLIC_IMAGEKIT_PRIVATE_KEY!,
+  urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
 });
 
 export default imagekit;
