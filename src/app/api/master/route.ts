4f654cfb4b4f687bda34eed543c24c250f6dfe5b
Arindam Mitra <arindammitra06@users.noreply.github.com>
2025-11-17 12:25:30 +0530

    Removed unused import

--- src/app/api/master/route.ts
+++ src/app/api/master/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { BooleanFlag } from "@prisma/client";
-import { uploadImageToImageKit } from "@/utils/uploadImageToImageKit";
 import { v4 as uuidv4 } from "uuid";
 import imagekit from "@/utils/imagekitClient";
 
